c5399357882f60a1b0749e3a79415264d5da7806
sergio <sergio_bandera@hotmail.com>
2022-04-19 14:05:06 +0200

    martes registro funcional

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,16 +2,38 @@ import React from 'react'
 import { useState } from 'react';
 
 export const Login = () => {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState(null);
   const [pass, setPass] = useState(null);  
   
   const cogerUser = (e) =>setUser(e.target.value);
   const cogerPass = (e) =>setPass(e.target.value);
 
   const login = () =>{
-  
-    console.log(user);
 
+    fetch ("http://localhost:8080/user/login",{
+        method: "POST",
+        headers:{
+          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        },
+        body: new URLSearchParams ({
+          "username": user,
+          "password": pass,
+        })
+
+      }).then((respuesta) => respuesta.json())
+      .then(data => {
+        if(data != null)console.log(data)
+      })
+      .catch("usuario no encontrado")
+
+              // if(respuesta.status===200){
+        //   console.log("Te has logeado correctamente")
+      
+        // }
+        // else{
+        //   console.log("Usuario no encontrado")
+          
+        // }
     }
    
   return (
@@ -22,7 +44,7 @@ export const Login = () => {
         <input type="text" name="username" placeholder="usuario" onChange={cogerUser}></input>
       </p>
       <p>
-        Contraseña<input type="password" placeholder="contraseña"></input>
+        Contraseña<input type="password" name="password" placeholder="contraseña" onChange={cogerPass}></input>
       </p>
       <button onClick={login}>Login</button>
       {/* </form> */}
--- src/components/Registrar.js
+++ src/components/Registrar.js
@@ -1,38 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Registrar = () => {
+  const panelUsuario = useNavigate();
 
-    const panelUsuario = useNavigate();
-    
-    const registarse = () =>{
+  const [usuario, setUsuario] = useState(null);
+  const [nombre, setNombre] = useState(null);
+  const [apellido, setApellido] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [password2, setPassword2] = useState(null);
 
-        console.log("te has registrado!!!")
-        panelUsuario("/PanelUsuario");
+  const cogerUsuario = (e) => setUsuario(e.target.value);
+  const cogerNombre = (e) => setNombre (e.target.value);
+  const cogerApellido = (e) => setApellido(e.target.value);
+  const cogerEmail = (e) => setEmail(e.target.value);
+  const cogerPassword = (e) => setPassword (e.target.value);
+  const cogerPassword2 = (e) => setPassword2 (e.target.value);
+
+
+  const registrarse = () => {
+    if (password != password2 ){
+      console.log("contraseña incorrecta")
+    }else if(usuario == null || nombre == null ||apellido == null ||
+       email == null || password == null || password2 == null){
+        console.log("los campos no pueden estar vacios")
     }
+    else{
+      fetch ("http://localhost:8080/user/signup",{
+        method: "POST",
+        headers:{
+          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        },
+        body: new URLSearchParams ({
+          "username": usuario,
+          "name": nombre,
+          "surname": apellido,
+          "email": email,
+          "password": password,
+          "passwordRepeated": password2
+        })
+
+      }).then(respuesta => console.log(respuesta))
+      .catch(function (err){
+        console.log(err);
+      })
+    }
+  };
 
   return (
     <>
       <h2>Datos de usuario</h2>
-      <form onSubmit={registarse}>
-      <p>
-        usuario
-        <input type="text" name="username" placeholder=" nombre de usuario"></input>
-      </p>
-      <p>
-        nombre<input type="text" placeholder="su nombre"></input>
-      </p>
-      <p>
-        apellido<input type="text" placeholder="su apellido"></input>
-      </p>
-      <p>
-        email<input type="email" placeholder="ponga su email"></input>
-      </p>
-      <p>
-        contraseña<input type="password" placeholder="una contraseña segura"></input>
-      </p>
-      <button type="submit">Registrarse</button>
-      </form>
+        <p>
+          usuario
+          <input
+            type="text"
+            name="username"
+            placeholder=" nombre de usuario"
+            onChange={cogerUsuario}
+          ></input>
+        </p>
+        <p>
+          nombre
+          <input type="text" 
+          placeholder="su nombre"
+          onChange={cogerNombre}
+          ></input>
+        </p>
+        <p>
+          apellido
+          <input type="text" 
+          placeholder="su apellido"
+          onChange={cogerApellido}
+          ></input>
+        </p>
+        <p>
+          email
+          <input type="text" 
+          placeholder="ponga su email"
+          onChange={cogerEmail}
+          ></input>
+        </p>
+        <p>
+          contraseña
+          <input type="password" 
+          placeholder="una contraseña segura"
+          onChange={cogerPassword}
+          ></input>
+          repita contraseña
+          <input type="password" 
+          placeholder="repita su contraseña"
+          onChange={cogerPassword2}
+          ></input>
+        </p>
+        <button onClick={registrarse}>Registrarse</button>
+  
     </>
   );
 };
